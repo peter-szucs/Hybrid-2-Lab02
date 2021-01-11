2944299224fbf3efc2c1ab26ea3edd8228208955
Peter Szücs <pepo_szucs@hotmail.com>
2021-01-11 01:11:51 +0100

    Done
    App Done

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { RoomComponent } from './roomcomponent'
+import { DeviceComponent } from './devicescomponent'
+
+const livingRoomImage = require("./assets/living-room.png");
+const bedroomImage = require("./assets/bed.png");
+const kitchenImage = require("./assets/kitchen.png");
+
+
 
 export default function App() {
+  const [deviceOne, setDeviceOne] = useState(false);
+  const [deviceTwo, setDeviceTwo] = useState(false);
+  const [deviceThree, setDeviceThree] = useState(false);
+  const [counter, setCounter] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.statusBar}></View>
+      <View style={styles.appInfoContainer}>
+        <Image style={{ width: 60, height: 60}}source={require("./assets/house.png")}></Image>
+        <Text style={textStyles.appTitle}>SmartHome</Text>
+      </View>
+      <Text style={textStyles.title}>Rooms</Text>
+      <View style={styles.roomContainer}>
+        <RoomComponent description="Living Room" image={livingRoomImage}></RoomComponent>
+        <RoomComponent description="Bedroom" image={bedroomImage}></RoomComponent>
+        <RoomComponent description="Kitchen" image={kitchenImage}></RoomComponent>
+      </View>
+      <Text style={textStyles.title}>Devices</Text>
+      <View style={{ padding: 20 }}>
+        <DeviceComponent description="Living Room Lamp" state={deviceOne} updateState={setDeviceOne} counter={counter} updateCounter={setCounter}></DeviceComponent>
+        <DeviceComponent description="Living Room Lamp" state={deviceTwo} updateState={setDeviceTwo} counter={counter} updateCounter={setCounter}></DeviceComponent>
+        <DeviceComponent description="Heater" state={deviceThree} updateState={setDeviceThree} counter={counter} updateCounter={setCounter}></DeviceComponent>
+        <Text style={textStyles.deviceCount}>Total devices On: {counter}</Text>
+      </View>
+      
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  appContainer: {
+    //flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  statusBar: {
+    height: 50,
+    backgroundColor: '#fff'
+  },
+  appInfoContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    paddingLeft: 20,
+    alignItems: 'center'
+  },
+  roomContainer: {
+    flexDirection: 'row',
+    padding: 10,
+  }
+
+});
+
+const textStyles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    paddingTop: 10,
+    paddingLeft: 20
+  },
+  appTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingLeft: 20,
+    color: '#146361'
+  },
+  deviceCount: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingTop: 15,
+  }
 });
---
+++ devicescomponent.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const LightAndName = ({ description, stateColor }) => {
+    if (stateColor) {
+        return (
+            <View style={styles.horizontal}>
+                <View style={ [styles.onOffBox, { backgroundColor: 'green' }]}></View>
+                <Text style={textStyle.deviceDescr}>{description}</Text>
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.horizontal}>
+                <View style={ [styles.onOffBox, { backgroundColor: 'red' }]}></View>
+                <Text style={textStyle.deviceDescr}>{description}</Text>
+            </View>
+        )
+    }
+}
+
+const OnOffButtons = ({ updateState, counter, updateCounter }) => {
+    return (
+        <View style={styles.vertical}>
+            <Button style={styles.buttons} title="On" onPress= {() => {
+                updateState(true)
+                updateCounter(counter + 1)
+            }} />
+            <Button style={styles.buttons} title="Off" onPress= {() => {
+                updateState(false)
+                updateCounter(counter - 1)
+            }} />
+        </View>
+    )
+}
+
+export function DeviceComponent({ description, state, updateState, counter, updateCounter }) {
+    return (
+        <View style={{ paddingBottom: 20 }}>
+            <View style={styles.main}>
+                <View style={styles.horizontal}>
+                    <LightAndName description={description} stateColor={state}></LightAndName>
+                    <OnOffButtons updateState={updateState} counter={counter} updateCounter={updateCounter}></OnOffButtons>
+                </View>
+            </View>
+        </View>
+        
+    )
+}
+
+
+const styles = StyleSheet.create({
+    main: {
+        backgroundColor: '#fff0a3',
+        padding: 10,
+    },
+    horizontal: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        
+    },
+    vertical: {
+        flexDirection: 'column',
+        alignContent: 'center',
+        backgroundColor: '#fff'
+    },
+    onOffBox: {
+        width: 20,
+        height: 20,
+        backgroundColor: 'red'
+    },
+    buttons: {
+        backgroundColor: '#ffffff',
+        borderWidth: 2,
+        borderColor: '#ffffff'
+    }
+});
+
+const textStyle = StyleSheet.create({
+    deviceDescr: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+});
